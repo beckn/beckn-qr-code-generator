9535756995270f00df26514d6d35cadc9094d9dd
Roshan George <roshangeorge2003@gmail.com>
2023-09-01 15:02:10 +0530

    Developed Interface for Generation and Interpretation

--- src/components/FormData.jsx
+++ src/components/FormData.jsx
@@ -1,74 +1,227 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import QRCode from "qrcode.react";
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from 'uuid';
 import { Link } from 'react-router-dom';
+import LoadingOverlay from './LoadingOverlay';
+import { useEffect } from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-blue-100 p-4 h-full">
+      {/* Logo */}
+      <div className="flex flex-col items-center">
+        <div className='m-4'><img src="src\assets\beckn-logo.png" alt="Logo"/></div>
+        <div><img src="src\assets\QR Code (1).gif" alt="Logo"/></div>
+      </div>
+    </nav>
+  );
+};
 
 const FormData = () => {
   const [responseMessage, setResponseMessage] = useState('');
   const [qrCodeDataURL, setQrCodeDataURL] = useState('');
-  const [jsondata,setData] = useState('');
+  const [jsondata, setData] = useState('');
+  const [categoryName, setCategoryName] = useState('');
+  const [searchString, setSearchString] = useState('');
+  const [providerId, setProviderId] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingMessage, setLoadingMessage] = useState('Loading...');
+  
 
   function generateUniqueId() {
     return uuid();
-}
+  }
 
   const postData = async () => {
+    setIsLoading(true);
+    setLoadingMessage('Loading...');
     const url = 'https://api-node-dev.mobilityreferencebap.becknprotocol.io/client/v2/search'; // Replace with your actual endpoint URL
 
     const data = {
-      
-        "context": {
-            "domain": "retail"
-        },
-        "message": {
-            "criteria": {
-                "dropLocation": "48.85041854,2.343660901",
-                "categoryName": "FoodEnglish",
-                "providerId": "./retail.kirana/ind.blr/101@retail-osm-prod.becknprotocol.io.provider"
-            }
+      "context": {
+        "domain": "retail"
+      },
+      "message": {
+        "criteria": {
+          "dropLocation": "48.85041854,2.343660901",
+          "categoryName": "FoodEnglish",
+          "providerId": providerId,
         }
-    
+      }
     };
-    
 
     await axios.post(url, data)
       .then(response => {
         console.log(response);
         const jsonData = response;
-        axios.post("http://127.0.0.1:3000/qrGenerator",jsonData)
-        .then(response=>{
-          console.log(response)
-          setQrCodeDataURL(response.data.uniqueId)
-        })
-        .catch(error => {
-          setResponseMessage('Error sending data');
-          console.error('Error sending data:', error);
-        });
+        axios.post("http://127.0.0.1:3000/qrGenerator", jsonData)
+          .then(response => {
+            console.log(response)
+            setQrCodeDataURL(response.data.uniqueId)
+            setIsLoading(false); // Set loading state to false when done
+
+          })
+          .catch(error => {
+            setResponseMessage('Error sending data');
+            console.error('Error sending data:', error);
+            setIsLoading(false); // Set loading state to false when done
+
+          });
       })
       .catch(error => {
         setResponseMessage('Error sending data');
         console.error('Error sending data:', error);
       });
-
-
-    
   };
 
+  const postSearchData = async () => {
+    setIsLoading(true);
+    setLoadingMessage('Loading...');
 
+    const url = 'https://api-node-dev.mobilityreferencebap.becknprotocol.io/client/v2/search'; // Replace with your actual endpoint URL
 
+    const data = {
+      "context": {
+        "domain": "retail"
+      },
+      "message": {
+        "criteria": {
+          "dropLocation": "48.85041854,2.343660901",
+          "categoryName": categoryName,
+          "providerId": providerId,
+          "searchString": searchString,
+        }
+      }
+    };
+
+    await axios.post(url, data)
+      .then(response => {
+        console.log(response);
+        const jsonData = response;
+        axios.post("http://127.0.0.1:3000/qrGenerator", jsonData)
+          .then(response => {
+            console.log(response)
+            setQrCodeDataURL(response.data.uniqueId)
+            setIsLoading(false); // Set loading state to false when done
+
+          })
+          .catch(error => {
+            setResponseMessage('Error sending data');
+            console.error('Error sending data:', error);
+            setIsLoading(false); // Set loading state to false when done
+
+          });
+      })
+      .catch(error => {
+        setResponseMessage('Error sending data');
+        console.error('Error sending data:', error);
+      });
+  };
+  const messages = ['Did you know? Beckn can enable over 120,000 different types of commerce transactions', 'Creating QR code...', 'Almost there...', 'Just a moment...'];
+
+  useEffect(() => {
+    if (isLoading) {
+      let index = 0;
+      const interval = setInterval(() => {
+        setLoadingMessage(messages[index]);
+        index = (index + 1) % messages.length;
+      }, 7000); // 10000 milliseconds = 10 seconds
+
+      return () => clearInterval(interval);
+    }
+  }, [isLoading]);
 
   return (
-    <div>
-      <div>
-        <button onClick={postData}>Create QR for my buisness</button>
-        <p>{responseMessage}</p>
-        {qrCodeDataURL && (
-          <div>
-              <Link to={`/ui/${qrCodeDataURL}`}>View QR Code Details</Link>
-              <QRCode value={`http://localhost:5173/ui/${qrCodeDataURL}`} style={{ marginRight: 50 }} />
+    <div className="flex">
+      <div className="w-2/4">
+        <Navbar />
+      </div>
+      <div className="w-2/4 p-4 bg-blue-100 flex justify-center items-center">
+  <div className="bg-blue-100 rounded-lg">
+          <div className="flex justify-between">
+            <div>
+            <label className="block mb-2">
+                Provider ID:
+                <input
+                  type="text"
+                  value={providerId}
+                  onChange={(e) => setProviderId(e.target.value)}
+                  className="border rounded py-2 px-3 w-full"
+                />
+              </label>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={postData}>
+              Create QR of my entire Catalogue
+            </button>
+            </div>
+         
+
+            <div className="w-2/3 ml-4">
+              <label className="block mb-2">
+                Category Name:
+                <input
+                  type="text"
+                  value={categoryName}
+                  onChange={(e) => setCategoryName(e.target.value)}
+                  className="border rounded py-2 px-3 w-full"
+                />
+              </label>
+
+              <label className="block mb-2">
+                Search String:
+                <input
+                  type="text"
+                  value={searchString}
+                  onChange={(e) => setSearchString(e.target.value)}
+                  className="border rounded py-2 px-3 w-full"
+                />
+              </label>
+
+              <label className="block mb-2">
+                Provider ID:
+                <input
+                  type="text"
+                  value={providerId}
+                  onChange={(e) => setProviderId(e.target.value)}
+                  className="border rounded py-2 px-3 w-full"
+                />
+              </label>
+
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={postSearchData}>
+                Create QR for a particular service
+              </button>
+
+              <p className="text-red-500 mt-2">{responseMessage}</p>
+            </div>
           </div>
-        )}
+          {isLoading && <LoadingOverlay message={loadingMessage} />}
+<div >
+{qrCodeDataURL && !isLoading && (
+  <div className="flex flex-col items-center mt-4">
+    <QRCode value={`http://localhost:5173/ui/${qrCodeDataURL}`} />
+    <div className="mt-2">
+      <Link
+        to={`/ui/${qrCodeDataURL}`}
+        className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded inline-block"
+      >
+        View QR Code Details
+      </Link>
+    </div>
+    <div className="mt-2">
+      <a
+        href={`http://localhost:5173/ui/${qrCodeDataURL}`}
+        download={`QR_Code_${qrCodeDataURL}.png`}
+        className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded inline-block"
+      >
+        Download QR Code
+      </a>
+    </div>
+  </div>
+)}
+
+
+</div>
+        </div>
       </div>
     </div>
   );
--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -4,32 +4,60 @@ import { useParams } from 'react-router-dom';
 
 const CatalogDisplay = ({ data }) => {
   return (
-    <div>
-      <h1>Fetched Data Display</h1>
+    <div className="p-4 bg-blue-100">
+      <h1 className="text-3xl font-semibold mb-6">Fetched Data Display</h1>
       {data ? (
         <div>
           {data.message.catalogs.map((catalog, index) => (
-            <div key={index}>
-              <h2>Catalog {index + 1}</h2>
-              <h3>Catalog Context</h3>
-              <pre>{JSON.stringify(catalog.context, null, 2)}</pre>
-              <h3>Providers</h3>
-              {catalog.message.catalog['bpp/providers'].map((provider, providerIndex) => (
-                <div key={providerIndex}>
-                  <h4>Provider {providerIndex + 1}</h4>
-                  <p>Provider Name: {provider.descriptor.name}</p>
-                  <h5>Items</h5>
-                  {provider.items.map((item, itemIndex) => (
-                    <div key={itemIndex}>
-                      <h6>Item {itemIndex + 1}</h6>
-                      <p>Item Name: {item.descriptor.name}</p>
-                      <p>Item Price: {item.price.listed_value} {item.price.currency}</p>
-                      <p>Item Description: {item.descriptor.short_desc}</p>
-                      <img src={item.descriptor.images[0]} alt={`Item ${itemIndex + 1}`} />
+            <div key={index} className="mb-8 bg-white p-4 rounded-lg shadow">
+              <h2 className="text-2xl font-semibold mb-4">Catalog {index + 1}</h2>
+              {catalog.message?.catalog ? (
+                <div>
+                  {/* ... (other catalog-related code) */}
+                  <h3 className="text-xl font-medium mt-4">Providers</h3>
+                  {catalog.message.catalog['bpp/providers'] ? (
+                    <div>
+                      {catalog.message.catalog['bpp/providers'].map((provider, providerIndex) => (
+                        <div key={providerIndex} className="mb-6 p-4 bg-gray-100 rounded-lg">
+                          {provider.items.map((item, itemIndex) => (
+                            <div key={itemIndex} className="flex mb-6 bg-white p-4 rounded-lg shadow">
+                              <div className="w-1/4">
+                                <img
+                                  src={item.descriptor.images[0]}
+                                  alt={`Item ${itemIndex + 1}`}
+                                  className="w-full h-auto"
+                                />
+                              </div>
+                              <div className="w-3/4 pl-4">
+                                <h6 className="text-lg font-semibold mb-2">{item.descriptor.name}</h6>
+                                <p className="mb-1">
+                                  Price: {item.price.listed_value} {item.price.currency}
+                                </p>
+                                <p className="mb-4">{item.descriptor.short_desc}</p>
+                                <div className="flex space-x-2 text-sm font-medium justify-start">
+                                  <button className="transition ease-in duration-300 inline-flex items-center text-sm font-medium mb-2 md:mb-0 bg-purple-500 px-5 py-2 hover:shadow-lg tracking-wider text-white rounded-full hover:bg-purple-600">
+                                    <span>Add Cart</span>
+                                  </button>
+                                  <button className="transition ease-in duration-300 bg-gray-700 hover:bg-gray-800 border hover:border-gray-500 border-gray-700 hover:text-white hover:shadow-lg text-gray-400 rounded-full w-9 h-9 text-center p-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                    </svg>
+                                  </button>
+                                </div>
+                              </div>
+                            </div>
+                          ))}
+                        </div>
+                      ))}
                     </div>
-                  ))}
+                  ) : (
+                    <p>No providers available for this catalog.</p>
+                  )}
                 </div>
-              ))}
+              ) : (
+                <p>No catalog data available.</p>
+              )}
             </div>
           ))}
         </div>
@@ -47,10 +75,8 @@ const UI = () => {
   useEffect(() => {
     async function fetchData() {
       try {
-        console.log(qrCodeDataURL);
         const response = await axios.get(`http://127.0.0.1:3000/getData/${qrCodeDataURL}`);
         setData(response.data.data);
-        console.log(response.data)
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -59,7 +85,11 @@ const UI = () => {
     fetchData();
   }, [qrCodeDataURL]);
 
-  return <CatalogDisplay data={data} />;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <CatalogDisplay data={data} />
+    </div>
+  );
 };
 
 export default UI;
