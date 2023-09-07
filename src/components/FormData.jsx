698a50d80d56c5fd8b91972332c43ed41bc6c7a2
Roshan George <roshangeorge2003@gmail.com>
2023-09-07 16:20:21 +0530

    deploy-errors2

--- src/components/FormData.jsx
+++ src/components/FormData.jsx
@@ -11,8 +11,8 @@ const Navbar = () => {
     <nav className="bg-blue-100 p-4 h-full">
       {/* Logo */}
       <div className="flex flex-col items-center">
-        <div className='m-4'><img src="public\beckn-logo.png" alt="Logo"/></div>
-        <div><img src="public\QR Code.gif" alt="Logo"/></div>
+        <div className='m-4'><img src="/beckn-logo.png" alt="Logo"/></div>
+        <div><img src="public/QR Code.gif" alt="Logo"/></div>
       </div>
     </nav>
   );
