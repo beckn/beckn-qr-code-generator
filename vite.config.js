dad081034a777863f177694fa789cdd66fdadb37
Roshan George <roshangeorge2003@gmail.com>
2023-09-08 21:22:46 +0530

    deploy-fix18

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    crossOriginIsolation()],
-  server: {
-    open: true,
-    origin: 'https://beckn-qr-141g.vercel.app',
-  },
-})
+  plugins: [
+    {
+      name: "configure-response-headers",
+      configureServer: (server) => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+          next();
+        });
+      },
+    },
+  ],
+});
