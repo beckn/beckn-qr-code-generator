7d4c62c2a9b724d81f8ab285d1f912775c644bc9
Roshan George <roshangeorge2003@gmail.com>
2023-09-08 21:08:23 +0530

    deploy-fix15

--- server/server.js
+++ server/server.js
@@ -70,3 +70,20 @@ app.get('/getData/:id', async (req, res) => {
         res.status(404).send("Data not found.");
     }
 });
+module.exports = (req, res) => {
+    //set header first to allow request or origin domain (value can be different)
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
+
+//---- other code
+
+ //Preflight CORS handler
+    if(req.method === 'OPTIONS') {
+        return res.status(200).json(({
+            body: "OK"
+        }))
+    }
+
+}
